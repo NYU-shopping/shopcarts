7e188b20124427562840c68f4456202949cc6335
sisi li <alicesqcg@gmail.com>
2018-04-30 02:42:40 +0800

    updated route

--- features/steps/shopcart_steps.py
+++ features/steps/shopcart_steps.py
@@ -42,7 +42,7 @@ def step_impl(context):
 @when(u'I visit the "Cart page"')
 def step_impl(context):
     """ Make a call to the base URL """
-    context.driver.get(context.base_url + '/shopcarts')
+    context.driver.get(context.base_url)
 
 @then(u'I should see "{message}" in the title')
 def step_impl(context, message):
